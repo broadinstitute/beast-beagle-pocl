fa38318af898c798705e7e2ca4fa8e8b1ecec51a
Danny Park <dpark@broadinstitute.org>
2018-04-13 16:49:20 -0400

    initial

---
+++ install-beagle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /opt/docker
+
+git clone https://github.com/beagle-dev/beagle-lib.git
+cd beagle-lib
+
+./autogen.sh
+./configure --prefix=/usr/local
+
+make
+
+make check
+
+make install
+
+examples/genomictest/genomictest
+examples/tinytest/tinytest
